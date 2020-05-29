1f40cede5d611a8c61f1680801c4dbf23314f2cb
rmlds <romualdaspaskevicius@gmail.com>
2020-05-29 22:07:15 +0100

    improve first test

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
+import unittest
 
 geckodriver_path = r'C:\Users\Romas\geckodriver\geckodriver.exe'
 
-browser = webdriver.Firefox(executable_path=geckodriver_path)
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):  
+    def setUp(self):  
+        self.browser = webdriver.Firefox(executable_path=geckodriver_path)
 
-assert 'Django' in browser.title
+    def tearDown(self):  
+        self.browser.quit()
+
+    def test_title(self):  
+        # Go to homepage
+        self.browser.get('http://127.0.0.1:8000')
+
+        # Check the title
+        self.assertIn('Rmlds | Personal Website | University Coursework', self.browser.title)  
+        self.fail('Finished the test!')  
+
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore')  
