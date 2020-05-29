59466076378078f07b44a8ec98246061a305c733
rmlds <romualdaspaskevicius@gmail.com>
2020-05-29 17:48:29 +0100

    finish up html and css

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,25 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
+from django.templatetags.static import static
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.CharField(max_length=30)
+    featured_image = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
+    @property
+    def image_url(self):
+        return static("images/{}".format(self.featured_image))
+
+    @property
+    def text_snippet(self):
+        snippet_length = 288
+        return self.text[0 : snippet_length] + '...'
 
     def publish(self):
         self.published_date = timezone.now()
