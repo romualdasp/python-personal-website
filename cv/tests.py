220d42048d43191271e155233dc5f8477971b050
rmlds <romualdaspaskevicius@gmail.com>
2020-05-30 11:49:20 +0100

    test returns correct html

--- cv/tests.py
+++ cv/tests.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from cv.views import cv_preview
 
 # Create your tests here.
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/cv/')  
+    def test_cv_url_resolves_to_cv_preview_view(self):
+        found = resolve('/cv/')
         self.assertEqual(found.func, cv_preview)
+
+    def test_cv_preview_returns_correct_html(self):
+        request = HttpRequest()
+        response = cv_preview(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('\n<html>'))
+        self.assertIn('<title>Rmlds | Personal Website | University Coursework</title>', html)
+        self.assertTrue(html.endswith('</html>\n'))
