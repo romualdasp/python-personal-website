53d6fe97e6eda5e1410fdfc715e8f92b36609893
rmlds <romualdaspaskevicius@gmail.com>
2020-05-30 12:38:37 +0100

    test using Django Test Client

--- cv/tests.py
+++ cv/tests.py
@@ -8,14 +8,24 @@ from cv.views import cv_preview
 
 class HomePageTest(TestCase):
 
+    # Manual way of testing
+
     def test_cv_url_resolves_to_cv_preview_view(self):
         found = resolve('/cv/')
         self.assertEqual(found.func, cv_preview)
 
     def test_cv_preview_returns_correct_html(self):
-        request = HttpRequest()
-        response = cv_preview(request)
+        response = self.client.get('/cv/')
+
         html = response.content.decode('utf8')
         self.assertTrue(html.strip().startswith('<html>'))
         self.assertIn('<title>Rmlds | Personal Website | University Coursework</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'cv/cv_preview.html')
+
+    # Testing using Django Test Client
+
+    def test_uses_cv_template(self):
+        response = self.client.get('/cv/')
+        self.assertTemplateUsed(response, 'cv/cv_preview.html')
